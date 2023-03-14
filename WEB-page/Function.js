5f8fd52b90e3a36b038d7e0d1e9707954e5d7c2a
Vincent Tse <king2003man2@gmail.com>
2023-03-14 18:01:37 +0800

    v0.2

--- WEB-page/Function.js
+++ WEB-page/Function.js
@@ -1,1 +1,18 @@
+let date = new Date()
+let getYear = date.getYear()+1900
+let selectYearDropDown = document.getElementById("selectYearDropDown")
+let selectMonthDropDown = document.getElementById("selectMonthDropDown")
 
+
+for(let y = getYear; y >= 1900; y--){
+    let createYear = document.createElement("option")
+    createYear.value = y
+    createYear.text = y
+    selectYearDropDown.appendChild(createYear)
+}
+for(let y = 1; y <= 12; y++){
+    let createMonth = document.createElement("option")
+    createMonth.value = y
+    createMonth.text = y
+    selectMonthDropDown.appendChild(createMonth)
+}
