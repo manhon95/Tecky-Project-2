7057c0c6f4ec5fe171e029cee049c715f7cba0cf
manhon95 <manhon95@yahoo.com.hk>
2023-03-20 22:45:26 +0800

    add bug fix suggestion in manageUserAccount.ts

--- manageUserAccount.ts
+++ manageUserAccount.ts
@@ -1,6 +1,22 @@
 import { Request, Response } from "express";
 import jsonfile from "jsonfile";
 
+/* --------------------------- bug fix suggestion --------------------------- */
+
+//import fs from "fs";
+
+// const filePath = "userDetail.json"; //filePath wil use several times
+
+// try {
+//   fs.accessSync(filePath, fs.constants.F_OK); //this function will return error if user file is not accessible
+// } catch (error) {
+//   fs.writeFileSync(filePath, JSON.stringify([])); // it error happen, create a new file with json empty array
+// }
+
+// export let users: User[] = jsonfile.readFileSync(filePath); //load the user array from user file, export them so that it can be reused in other files
+
+/* ----------------------------------- end ---------------------------------- */
+
 let users: User[] = jsonfile.readFileSync("userDetail.json");
 type User = {
   title: string;
@@ -22,45 +38,41 @@ export async function saveUserDetails(req: Request, res: Response) {
   let yearOfBirth = req.body.yearOfBirth;
   let email = req.body.email;
   let password = req.body.password;
-  let confirmPassword = req.body.confirmPassword
+  let confirmPassword = req.body.confirmPassword;
   let emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  let report = {}
-  
+  let report = {};
+
   // let body = req.body
   //  for(let i in body){
   // }
 
-  if(password.length<8){
-          res.status(400)
-          report["passwordLength-password"] = "Password must be 8 or more character*"
-          checkStatus = false;
-  }else{
-    report["passwordLength-password"] = ""
-
-  }
-  if(password!=confirmPassword){
-    report["pwCheck-confirmPassword"] = "Confirm password is different*"
+  if (password.length < 8) {
+    res.status(400);
+    report["passwordLength-password"] = "Password must be 8 or more character*";
     checkStatus = false;
-}else{
-  report["pwCheck-confirmPassword"] = ""
+  } else {
+    report["passwordLength-password"] = "";
+  }
+  if (password != confirmPassword) {
+    report["pwCheck-confirmPassword"] = "Confirm password is different*";
+    checkStatus = false;
+  } else {
+    report["pwCheck-confirmPassword"] = "";
+  }
 
-}
-
-
-  if(!email.match(emailFormat)){
-report["Format-email"] = "invalid Email*"
-   checkStatus = false;
-  }else{
-    report["Format-email"] = ""
-
+  if (!email.match(emailFormat)) {
+    report["Format-email"] = "invalid Email*";
+    checkStatus = false;
+  } else {
+    report["Format-email"] = "";
   }
   for (let user of users) {
-      if (user.email == email) {
-        report["duplicate-email"] = "Email already used*"
-        checkStatus = false;
-      continue
-    }else{
-      report["duplicate-email"] = ""
+    if (user.email == email) {
+      report["duplicate-email"] = "Email already used*";
+      checkStatus = false;
+      continue;
+    } else {
+      report["duplicate-email"] = "";
     }
   }
   if (checkStatus) {
@@ -71,12 +83,12 @@ report["Format-email"] = "invalid Email*"
       monthOfBirth,
       yearOfBirth,
       email,
-      password
+      password,
     });
     await jsonfile.writeFile("userDetail.json", users);
     res.end("done");
   } else {
-    res.status(400)
-    res.json(report)
+    res.status(400);
+    res.json(report);
   }
 }
