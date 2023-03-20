47d5777a684d6b2ee6ae7b18a624de6c4fd1cf56
VIctor Law <manhon95@gmail.com>
2023-03-20 17:16:46 +0800

    add redirect function to main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,15 @@
-import express, { Request, Response } from 'express'
-import {saveUserDetails} from "./manageUserAccount"
-import {passwordChecker} from "./LoginAuthenticate"
-import {print} from "listening-on"
+import express, { Request, Response } from "express";
+import { saveUserDetails } from "./manageUserAccount";
+import { passwordChecker } from "./LoginAuthenticate";
+import { print } from "listening-on";
+import path from "path";
 // import Session from "express-session";
 //  import {}
-let app = express()
+let app = express();
 
-app.use(express.static("public"))
-app.use(express.urlencoded())
-app.use(express.json())
+app.use(express.static("public"));
+app.use(express.urlencoded());
+app.use(express.json());
 // app.use(Session({
 //       secret: Math.random().toString(36),
 //       resave: false,
@@ -22,16 +23,23 @@ app.use(express.json())
 //   }
 
 // app.post()
-app.post("/register", (req: Request,res: Response)=>{
- saveUserDetails(req, res)
-})
-app.post("/login", (req: Request,res: Response)=>{
-  passwordChecker(req, res)
- })
- 
+app.post("/register", (req: Request, res: Response) => {
+  saveUserDetails(req, res);
+});
+app.post("/login", (req: Request, res: Response) => {
+  passwordChecker(req, res);
+});
+
+app.get("/login", (req: Request, res: Response) => {
+  res.sendFile(path.join("public", "login-page.html"));
+});
+
+app.use((req: Request, res: Response) => {
+  res.redirect("/login");
+});
 
 // app.get("/submit" )
-const PORT = 8080
+const PORT = 8080;
 app.listen(PORT, () => {
- print(PORT)
-})
+  print(PORT);
+});
