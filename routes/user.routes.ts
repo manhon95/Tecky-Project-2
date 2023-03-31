dd9e5647eec3d77bbd67d817b6436b49d6a105d3
Ivan Yiu <ivanyiu2017@gmail.com>
2023-03-31 13:44:15 +0800

    remove duplicated code

--- routes/user.routes.ts
+++ routes/user.routes.ts
@@ -57,32 +57,6 @@ userRoutes.put("/ProfilePic/:id", async (req, res) => {
 
 userRoutes.use("/profilePic", express.static(uploadDir));
 
-const uploadDir = "profilePicture";
-fs.mkdirSync(uploadDir, { recursive: true });
-const form = formidable({
-  uploadDir,
-  keepExtensions: true,
-  filter: (part) => part.mimetype?.startsWith("image/") || false,
-});
-
-userRoutes.put("/ProfilePic/:id", async (req, res) => {
-  let id = req.params.id;
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      res.status(507);
-      res.json({ err: "fail to up load image", detail: String(err) });
-      return;
-    }
-    let profilePic = files.profilePic;
-
-    let image = Array.isArray(profilePic) ? profilePic[0] : profilePic;
-    console.log(image.newFilename);
-    res.json(image.newFilename);
-  });
-});
-
-userRoutes.use("/profilePic", express.static(uploadDir));
-
 userRoutes.get("/profilePic", async (req, res) => {
   if (req.session.user) {
     let ProfilePic = await getProfilePic(req.session.user.id);
